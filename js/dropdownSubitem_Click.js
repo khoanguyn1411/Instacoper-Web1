0a56fc1431c48a40e212837b2b97d122ba7c0415
BryantTranGB <baothg19411c@st.uel.edu.vn>
2022-01-08 20:47:58 +0700

    add Toggle Subnavigation && User on header

--- js/dropdownSubitem_Click.js
+++ js/dropdownSubitem_Click.js
@@ -4,12 +4,12 @@ function dropdowncart(){
 
   window.onclick = function(event) {
 
-    const isSubcart = event.target.closest('#cart')
+    const isSubcart = event.target.match('#subcart')
 
-    if (isSubcart && event.target.matches("#subcart")!=null ){
+    if (isSubcart){
      return;
     } 
-    if(!event.target.matches('#subcart')){
+    if(!event.target.closest('#subcart')){
       var dropdowns = document.getElementsByClassName('subcart');
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -20,3 +20,36 @@ function dropdowncart(){
       }
     }
   }
+
+
+  // const $menu = $('.subcart');
+
+  // $(document).mouseup(e => {
+  //    if (!$menu.is(e.target) // if the target of the click isn't the container...
+  //    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+  //    {
+  //      $menu.removeClass('show');
+  //   }
+  //  });
+  
+  // $('#cart').on('click', () => {
+  //   $menu.toggleClass('show');
+  // });
+  
+
+
+
+// document.addEventListener('click', Event =>{
+//   const isToggleIcon = Event.target.matches("[data-toggle-cart-icon]")
+//   if(!isToggleIcon && Event.target.closest('[data-toggle-cart]') !=null) return
+  
+//   let currentToggle
+//   if(isToggleIcon){
+//     currentToggle = e.target.closest('[data-toggle-cart-icon]')
+//     currentToggle.classList.toggle('actived')
+//   }
+//   document.querySelectorAll("[data-toggle-cart].actived").forEach(isToggleIcon => {
+//      if(isToggleIcon === currentToggle) return
+//      isToggleIcon.classList.remove('actived')
+//   })
+// })
