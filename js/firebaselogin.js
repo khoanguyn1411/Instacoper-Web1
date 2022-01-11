f28c6481a6c17b4c385bd94ba87c6a75559263a3
BryantTranGB <baothg19411c@st.uel.edu.vn>
2022-01-11 19:56:10 +0700

    link firebase signin signup && add admin dashboard page version 1

---
+++ js/firebaselogin.js
@@ -0,0 +1,78 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-analytics.js";
+  import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   const firebaseConfig = {
+    apiKey: "AIzaSyBrZQsPWWwPxy3BTuBmCb7Zi99lgIfh28A",
+    authDomain: "instacoper-app.firebaseapp.com",
+    databaseURL: "https://instacoper-app-default-rtdb.firebaseio.com",
+    projectId: "instacoper-app",
+    storageBucket: "instacoper-app.appspot.com",
+    messagingSenderId: "251358459774",
+    appId: "1:251358459774:web:a533a5bccfb8ae352ce52f",
+    measurementId: "G-HW0GKH9YQL"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const database = getDatabase(app);
+  const auth = getAuth();
+
+
+
+
+document.getElementById('loginbtn').addEventListener("click" ,(e)=>{
+    var email = document.getElementById('txtEmailL').value;
+    var password = document.getElementById('txtPasswordL').value;
+  
+   signInWithEmailAndPassword(auth, email, password)
+   .then((userCredential) => {
+     // Signed in 
+     const user = userCredential.user;
+  
+     const dt = new Date();
+      update(ref(database, 'users/' + user.uid),{
+       last_login: dt,
+     })
+     location.replace("member.html")
+      alert('Đăng nhập thành công');
+     // ...
+   })
+   .catch((error) => {
+     const errorCode = error.code;
+     const errorMessage = error.message;
+  
+     alert(errorMessage);
+  });
+  
+  });
+
+//   const user = auth.currentUser;
+
+//   onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//         link
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/firebase.User
+//     //   const uid = user.uid;
+//         location.replace("blog.html")
+//       // ...
+//     } else {
+//       // User is signed out
+//       // ...
+//        return
+//     }
+//   });
+
+// firebase.auth().onAuthStateChanged((user)=>{
+//     if(user){
+//     }
+// })
---
+++ js/firebaseregister.js
@@ -0,0 +1,68 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-analytics.js";
+  import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   const firebaseConfig = {
+    apiKey: "AIzaSyBrZQsPWWwPxy3BTuBmCb7Zi99lgIfh28A",
+    authDomain: "instacoper-app.firebaseapp.com",
+    databaseURL: "https://instacoper-app-default-rtdb.firebaseio.com",
+    projectId: "instacoper-app",
+    storageBucket: "instacoper-app.appspot.com",
+    messagingSenderId: "251358459774",
+    appId: "1:251358459774:web:a533a5bccfb8ae352ce52f",
+    measurementId: "G-HW0GKH9YQL"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const database = getDatabase(app);
+  const auth = getAuth();
+
+
+
+  document.getElementById('registerbtn').addEventListener("click" ,(e) => {
+
+
+      var email = document.getElementById('txtEmailR').value;
+      var password = document.getElementById('txtPasswordR').value;
+      var username = document.getElementById('txtNameR').value;
+      var phonenumber = document.getElementById('txtPhoneR').value;
+
+      createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+   // Signed in 
+    const user = userCredential.user;
+
+    set(ref(database, 'users/' +user.uid),{
+
+      username: username, 
+      email: email,
+      password: password,
+      phonenumber: phonenumber
+    })
+
+
+    alert('Đăng ký thành công');
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    alert(errorMessage);
+  // ..
+  });
+
+});
+
+
+
