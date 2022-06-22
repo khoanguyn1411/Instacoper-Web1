fba05ef9fd8a218fb5cbebbf60ffa2337c7ca5a3
khoanguyn1411 <khoaah1411@gmail.com>
2022-06-23 02:01:16 +0700

    Add vercel href to nav

--- js/rangeslider.js
+++ js/rangeslider.js
@@ -24,8 +24,6 @@ rangeInput.forEach(input =>{
             progress.style.left = (minVal/rangeInput[0].max) * 100 + "%";
             progress.style.right = 100 - (maxVal/rangeInput[1].max) * 100 + "%";
         }
-       
-
     });
 });
 
